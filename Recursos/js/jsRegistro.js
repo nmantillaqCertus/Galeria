74f02bc40b5c533bafbe3dac47b42dd483cafbda
nmantillaqCertus <90099252+nmantillaqCertus@users.noreply.github.com>
2022-05-02 08:43:46 -0500

    clase 02-05 v1

--- Recursos/js/jsRegistro.js
+++ Recursos/js/jsRegistro.js
@@ -3,26 +3,100 @@ function fnEnviar(){
     /*Derando variables */
     var vNombre = vApellido = vCorreo = vInstitucion = "";
 
-
     /*Asignando variables */
-    vNombre = document.getElementById("idNombre").value;
+    //vNombre = document.getElementById("idNombre").value;
+    vNombre = $("#idNombre").val();
     vApellido = document.getElementById("idApellido").value;
     vCorreo = document.getElementById("idCorreo").value;
     vInstitucion = document.getElementById("idInstitucion").value;
+
+    /*Insertando datos a la vista html del modal*/
+    //document.getElementById("sNombre").innerHTML = vNombre;
+    $("#sNombre").text(vNombre);
+
     
 
-
-    /*Insertando datos a la vista html */
-    document.getElementById("sNombre").innerHTML = vNombre;
     document.getElementById("sApellido").innerHTML = vApellido;
     document.getElementById("sCorreo").innerHTML = vCorreo;
     document.getElementById("sInstitucion").innerHTML = vInstitucion;
 
+    /*
+    if(condicion){
+        hacer algo
+    }
+
+    if(condicion){
+        Hacer Cosa Uno
+    }else{
+        Hacer Cosa DOS
+    }
+    */
+   if(validaCampos() == true){
+        /*var Modalhtml = document.getElementById("mRegistro");
+        var MiModal = new bootstrap.Modal(Modalhtml);
+        MiModal.show();*/
+
+        $("#mRegistro").modal("show");
+        //$("#mRegistro").modal("hide");
+   }
+
     
-    console.log("Nombre: "+vNombre 
-    + " Apellido: "+vApellido 
-    + " Correo: "+vCorreo
-    + "Institución :"+vInstitucion);
+}
+
+//true =  verdadero
+// false =  falso
+
+function validaCampos(){
+   var valor = true;
+   var vNombre = document.getElementById("idNombre").value;   
+   var vApellido = document.getElementById("idApellido").value;
+   var vCorreo = document.getElementById("idCorreo").value;
+   var vInstitucion = document.getElementById("idInstitucion").value;
+
+   //Objetos
+
+   debugger
+
+   var ObjNombre = document.getElementById("idErrorNombre");
+   var ObjApellido = document.getElementById("idErrorApellido");
+   var ObjCorreo = document.getElementById("idErrorCorreo");
+   var ObjInstitucion = document.getElementById("idErrorInstitucion");
+
+    if(vNombre.trim().length == 0){
+        valor = false;
+        //Mostrar mensaje de error
+        ObjNombre.style.display = "block";
+        document.getElementById("idNombre").focus();
+    }else{
+       // Ocultar mensaje de error.
+       ObjNombre.style.display = "none";
+    }
+
+    if(vApellido.trim().length == 0){
+        valor = false
+        ObjApellido.style.display = "block";
+        document.getElementById("idApellido").focus();        
+    }else{
+        ObjApellido.style.display = "none";
+    }
+
+    if(vCorreo.trim().length == 0){
+        valor = false
+        ObjCorreo.style.display = "block";
+        document.getElementById("idCorreo").focus();
+    }else{
+        ObjCorreo.style.display = "none";
+    }
+
+    if(vInstitucion.trim().length == 0){
+        valor = false
+        ObjInstitucion.style.display = "block";
+        document.getElementById("idInstitucion").focus();
+    }else{
+        ObjInstitucion.style.display = "none";
+    }
+
+    return valor;
 }
 
 
