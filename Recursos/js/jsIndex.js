46f0322fd60d3d74a20e461e0bd7ff55675856a8
nmantillaqCertus <90099252+nmantillaqCertus@users.noreply.github.com>
2022-05-16 08:05:56 -0500

    clase 16 - 05

---
+++ Recursos/js/jsIndex.js
@@ -0,0 +1,134 @@
+$( document ).ready(function() {
+    console.log("Prueba");/// llegas hasta acá
+    action();
+    getData();
+    //$("#idModalBienvenida").show();
+});
+
+//Declaración de variables
+var Alumno = [];
+
+
+function action(){
+    Alumno = [{
+        nombre : "Cesar",
+        Apellido: "Mantilla",
+        Edad: 30,
+        Direccion: "Tulipanes",
+        Hobby:{
+            Deporte: "Play Station",
+            Musica: "All"
+        },
+        salario: function(){
+            var resultado = 0;
+            if(typeof(this.Edad) == "string"){
+                resultado = parseInt(this.Edad) * 1000;
+            }else{
+                resultado = this.Edad * 1000;
+            }
+            return resultado
+        }
+    },
+    {
+        nombre : "Cesar2",
+        Apellido: "Mantilla2",
+        Edad: 30,
+        Direccion: "Tulipanes2",
+        Hobby:{
+            Deporte: "Play Station2",
+            Musica: "All2"
+        },
+        salario: function(){
+            var resultado = 0;
+            if(typeof(this.Edad) == "string"){
+                resultado = parseInt(this.Edad) * 2000;
+            }else{
+                resultado = this.Edad * 2000;
+            }
+            return resultado
+        }
+    },
+    {
+        nombre : "Cesar3",
+        Apellido: "Mantilla3",
+        Edad: 30,
+        Direccion: "Tulipanes3",
+        Hobby:{
+            Deporte: "Play Station3",
+            Musica: "All3"
+        },
+        salario: function(){
+            var resultado = 0;
+            if(typeof(this.Edad) == "string"){
+                resultado = parseInt(this.Edad) * 3000;
+            }else{
+                resultado = this.Edad * 3000;
+            }
+            return resultado
+        }
+    }
+    ]
+
+
+
+
+    if(Alumno.length > 0){
+
+        //console.log(Alumno);
+        fnItearar(Alumno)
+    }
+}
+
+
+
+function fnItearar(arr){
+    for(let a=0; a<arr.length; a++){
+        
+        console.log("Hola " + arr[a].nombre);
+    }
+}
+
+
+function getData(){
+    //idItems
+    var settings = {
+    "url": "https://api.publicapis.org/entries",
+    "method": "GET",
+    "timeout": 0,
+    };
+    
+    $.ajax(settings).done(function (response) {
+    console.log(response);
+    });
+}
+
+
+
+
+
+
+
+
+
+///VARIABLES
+
+/*
+var ejemploVariable = ""; // Tipo de dato String
+var ejemploVariable = 2; // Tipo de dato Number
+var ejemploVariable = true; // Tipo de dato boolean
+var ejemploVariable = []; // Tipo de dato Array
+var ejemploVariable = {}; // Tipo de dato Objet
+
+var ejemploVariable = function(){}; // Tipo de funcion anonima
+
+function funcion(){} //Tipo de Dato Funcion 
+function funcion(val){return val} //Tipo de Dato Funcion con retorno
+
+for(){} // bucles
+if(){} // Condicionales
+onclick //Evento de clic
+onblur // 
+onchange
+
+*/
+
